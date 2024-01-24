818f6a0bad7ce0f51ca74148fc2f4f7f63ab0b6e
diniat <dianatobe@gmail.com>
2024-01-23 21:36:37 -0300

    added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Inter, Lato } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import React from "react";
 import Providers from "./lib/utils/Providers";
@@ -7,14 +7,6 @@ import { Toaster } from "@/components/ui/toaster";
 import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
-
-export const lato = Lato({
-  weight: ["300", "400", "700", "900"],
-  style: ["normal", "italic"],
-  subsets: ["latin"],
-  display: "swap",
-  variable: "--font-lato",
-});
 
 export const metadata: Metadata = {
   title: "CineQuest",
@@ -27,8 +19,28 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={lato.className}>
-      <body>
+    <html lang="en">
+      <head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
+      <body className={inter.className}>
         <Header />
         <Providers>{children}</Providers>
         <Toaster />
